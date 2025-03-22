045c1b55f6f1db040394b9ec638ae818aec83632
miyuppie <yemokanlawon@gmail.com>
2025-03-22 12:36:23 +0100

    email

--- paymentEndpoint/settings.py
+++ paymentEndpoint/settings.py
@@ -60,13 +60,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'paymentEndpoint.wsgi.application'
 
 # Email configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+
 # PayPal configuration
 PAYPAL_MODE = 'sandbox'
 PAYPAL_CLIENT_ID = os.getenv('PAYPAL_CLIENT_ID', '')
