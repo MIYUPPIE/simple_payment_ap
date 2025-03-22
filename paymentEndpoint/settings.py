f62c3aa1f2c152cd7c5b6f1d0f2b37d2c055d758
miyuppie <yemokanlawon@gmail.com>
2025-03-22 10:23:45 +0100

    update requirements.txt for Railway deployment

--- paymentEndpoint/settings.py
+++ paymentEndpoint/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import environ
 import os
 from pathlib import Path
-
+# gunicorn paymentEndpoint.wsgi:application
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
